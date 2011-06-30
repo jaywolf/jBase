277484104c5215cb8f04f84709997dec357e74c0
J Wolf <wolf.jay@gmail.com>
2011-06-30 10:10:11 -0700

    removing type and cufon

--- type/cufon/README.txt
+++
@@ -1,8 +0,0 @@
-
-Cufon documentation at:
-http://wiki.github.com/sorccu/cufon
-
-Generate fonts at:
-http://cufon.shoqolate.com/generate/
-
-
--- type/cufon/theme_name.cufon.js
+++
@@ -1,9 +0,0 @@
-Drupal.behaviors.cufon = function (context) {
-  Cufon.replace('h2.title', {
-    textShadow: '-2px -1px #3b291e'
-  });
-  Cufon.replace('.primary-menu-inner ul.sf-menu a', {
-    hover: true,
-    textShadow: '-2px -1px #3b291e'
-  });
-};
