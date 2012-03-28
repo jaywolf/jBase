137b3e6e66f7d423b442a8f23731b64673e85934
Jay Wolf <wolf.jay@gmail.com>
2012-03-28 14:58:17 -0700

    fixing XSS issue w/ body classes: using drupal_clean_css_identifier() function to clean $path_alias

--- template.php
+++ template.php
@@ -33,7 +33,7 @@ function jBase_preprocess_page(&$vars) {
   }
   
   // Give <body> tag a unique id depending on PAGE PATH
-  $path_alias = strtolower(preg_replace('/[^a-zA-Z0-9-]+/', '-', drupal_get_path_alias($_GET['q'])));
+  $path_alias = strtolower(jBase_clean_css_identifier(drupal_get_path_alias($_GET['q'])));
   if ($path_alias == 'node') {
     $vars['body_id'] = 'page-front';
   }
@@ -225,3 +225,35 @@ function jBase_theme_paths($theme) {
   }
   return array_reverse($all_parents);
 }
+
+
+/**
+ * @function _clean_css_identifier()
+ *   backport of drupal_clean_css_identifier() from Drupal 7.x
+ *
+ * @param $identifier
+ *   the identifier to clean
+ * @param $filter
+ *   an array of string replacements to use on the identifier 
+ *
+ * @return
+ *   A string safe for use as a CSS class or ID
+ **/
+ function jBase_clean_css_identifier($identifier, $filter = array(' ' => '-', '_' => '-', '/' => '-', '[' => '-', ']' => '')) {
+ 
+   // By default, we filter using Drupal's coding standards.
+   $identifier = strtr($identifier, $filter);
+ 
+   // Valid characters in a CSS identifier are:
+   // - the hyphen (U+002D)
+   // - a-z (U+0030 - U+0039)
+   // - A-Z (U+0041 - U+005A)
+   // - the underscore (U+005F)
+   // - 0-9 (U+0061 - U+007A)
+   // - ISO 10646 characters U+00A1 and higher
+   // We strip out any character not in the above list.
+   $identifier = preg_replace('/[^\x{002D}\x{0030}-\x{0039}\x{0041}-\x{005A}\x{005F}\x{0061}-\x{007A}\x{00A1}-\x{FFFF}]/u', '', $identifier);
+ 
+   return $identifier;
+ 
+ }
